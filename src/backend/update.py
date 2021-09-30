3f9b5d2fa58a7d62ed69b84f6df74dec012a4deb
xanjali <46597867+xanjali@users.noreply.github.com>
2021-09-30 15:44:43 +0530

    covid status update file

--- src/backend/update.py
+++ src/backend/update.py
@@ -4,12 +4,12 @@ d1= pd.read_csv("data_table1.csv")
 
 u="18b535" #user id from flutter app
 
-#----covid status update and listing of details-----
+#----covid status update if a person is covid + ve -----
 for i in range(len(d1)):
     #-----update covid status--------
     if(d1.loc[i,"User_id"]==u):
        d1.loc[i,"Covid_status"]=1
-       # updating csv file
+       #---- updating csv file -------
        d1.to_csv("data_table1.csv",index=False)
        print("Covid affected person : ",d1.loc[i,"Name"])
        print("Covid Status update sccessfully....")
