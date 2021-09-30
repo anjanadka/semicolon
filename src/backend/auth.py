161c72c37913d37065398f9be3dd3ba35266b125
xanjali <46597867+xanjali@users.noreply.github.com>
2021-09-30 15:34:23 +0530

    auth.py

--- src/backend/auth.py
+++ src/backend/auth.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
 d1= pd.read_csv("data_table1.csv")
-u="18b535"
-p="ai123"
 
+u="18b535" # user id from flutter app
+p="ai123"  # password from flutter app
+f=0
 for i in range(len(d1)):
     if(d1.loc[i,"User_id"]==u and d1.loc[i,"Password"]==p):
-        print(d1.loc[i,"Name"]) 
+        # user Log in sccessfull
+        print("User_id : ",u,"Name : ",d1.loc[i,"Name"]) 
+        print("Login Success......")
+        f=1
+if f==0:
+    # user Log in Failed
+    print("Invalid User_id or Password ......")
+    print("Please check again ......")
